aa82262b9596f2e3e91a2fd4df8d89273343117c
Tyre88 <victor@webbdudes.se>
2016-03-19 09:03:35 +0100

    Adding AppsettingsHelper and EmailHelper

---
+++ Helpers/AppSettingsHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradera.Core.Helpers
+{
+    public static class AppSettingsHelper
+    {
+        public static string GetAppSetting(string key)
+        {
+            string settingValue = string.Empty;
+            System.Configuration.Configuration rootWebConfig =
+                System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration(null);
+            if (rootWebConfig.AppSettings.Settings.Count > 0)
+            {
+                System.Configuration.KeyValueConfigurationElement customSetting =
+                    rootWebConfig.AppSettings.Settings[key];
+                if (customSetting != null)
+                    settingValue = customSetting.Value;
+            }
+            return settingValue;
+        }
+    }
+}
---
+++ Helpers/EmailHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradera.Core.Helpers
+{
+    public class EmailHelper
+    {
+        public static void SendEmail(string email, string emailHtml, string emailSubject)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient client = new SmtpClient();
+
+            client.Port = 587;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Host = AppSettingsHelper.GetAppSetting("Smtp");
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential(AppSettingsHelper.GetAppSetting("SmtpUserName"),
+                AppSettingsHelper.GetAppSetting("SmtpPassword"));
+
+            mail.To.Add(email);
+            mail.From = new MailAddress(AppSettingsHelper.GetAppSetting("EmailFrom"));
+            mail.Body = emailHtml;
+            mail.Subject = emailSubject;
+            mail.IsBodyHtml = true;
+
+            client.Send(mail);
+        }
+    }
+}
